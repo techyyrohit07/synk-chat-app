2f6e45f9b6a88b45d032bdda5b7c0ec51dde781f
Rohit Singh <rohitsinghnegi910@gmail.com>
2025-10-22 20:58:25 +0530

    Refactored code for using Redux persist

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,67 +6,79 @@ import ChatArea from "../components/layout/ChatArea";
 import RightSideBar from "../components/layout/RightSideBar";
 import { getUserChats } from "../services/chatService";
 import { getMessages, sendMessage } from "../services/messageService";
-import { loginUser } from "../services/authService";
 import { joinUser, sendSocketMessage, socket } from "../sockets/socket.js";
 import { cleanUpListeners, registerSocketListeners } from "../sockets/socketEvents.js";
 import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchChats, setChatStatus } from "../store/slices/chatSlice.js";
+import { addMessage, fetchMessages, sendNewMessage } from "../store/slices/messageSlice.js";
 
 
 
 const HomePage = () => {
 
-  const [chats, setChats] = useState([])
-  const [activeChat, setActiveChat] = useState(null)
-  const [messages, setMessages] = useState([])
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user)
 
-  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  const {chatList : chats , activeChat} = useSelector((state) => state.chats)
+  const {messages : messages} = useSelector((state) => state.messages)
+
   const activeChatRef = useRef(activeChat)
-
-  useEffect(() => {
-    (async () => {
-      const data = await getUserChats()
-      setChats(data)
-    })()
-  }, [])
 
   useEffect(() => {
     activeChatRef.current = activeChat
   }, [activeChat])
 
-  useEffect(() => {
-    if(user._id){
-      joinUser(user._id)
-      registerSocketListeners(socket,activeChatRef, setMessages)
-    }
+   useEffect(() => {
+    dispatch(fetchChats()).then(() => {
+      if(activeChat){
+        console.log("Restore messages for persisting chat :", activeChat._id);
+        dispatch(fetchMessages(activeChat._id))
+        
+      }
+    })
+  }, [dispatch])
 
-    return () => cleanUpListeners(socket)
-  }, [user._id])
+  useEffect(() => {
+    if(!user?._id) return
+    console.log("User Id :", user._id);
+    
+    joinUser(user._id)
+    registerSocketListeners(socket,activeChatRef, (msg) =>{
+        dispatch(addMessage(msg))
+      })  
+
+    return () => {
+      cleanUpListeners(socket)
+    }
+  }, [user?._id, dispatch])
 
   
   async function handleSelectChat(chat){
-    setActiveChat(chat)
-    console.log(chat);
+    dispatch(setChatStatus(chat))
+    // console.log("Chats : ", chat);
     
-    const data = await getMessages(chat._id)
-    setMessages(data)
-    console.log("Message :" ,data);
-    
-    
+    dispatch(fetchMessages(chat._id))
   }
 
   async function handleSendMessage(text){
     if(!activeChat) return
-    const newMsg = await sendMessage(activeChat._id, text)
-    setMessages((prev) => [...prev, newMsg])
+    try {
+      const newMsg = await dispatch(sendNewMessage({chatId : activeChat._id, text})).unwrap()
+    
 
-    const receiver = await activeChat.participants.find((p) => p._id !== user._id)
+      const receiver = activeChat.participants.find((p) => p._id !== user._id)
 
-    sendSocketMessage({
-      chatId : activeChat._id,
-      senderId : user._id,
-      receiverId : receiver._id,
-      text
+      sendSocketMessage({
+        chatId : activeChat._id,
+        senderId : user._id,
+        receiverId : receiver._id,
+        text
     })
+    } catch (error) {
+      console.log("Failed to send messages : ", error.message);
+      
+    }
   }
   
   return (
