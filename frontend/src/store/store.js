3a52b93afa0e98736a2a48f5be28ead501d46944
Rohit Singh <rohitsinghnegi910@gmail.com>
2025-10-22 21:04:42 +0530

    Add persistStore in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,31 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice.js'
 import chatReducer from './slices/chatSlice.js'
 import messageReducer from './slices/messageSlice.js'
+import {persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key : 'root',
+    storage,
+    whitelist : ["user", "chats"]
+}
 
-const store = configureStore({
-    reducer : {
-        user :  userReducer,
-        chats : chatReducer,
-        messages : messageReducer
-    }
+const rootReducer = combineReducers({
+    user :  userReducer,
+    chats : chatReducer,
+    messages : messageReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer : persistedReducer,
+    middleware : (getDefaultMiddleWare) => 
+        getDefaultMiddleWare({
+            serializableCheck : false
+        })
+})
+
+export const persistor = persistStore(store)
 export default store
