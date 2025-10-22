cddde04e7e60d8fbe504c9e081c992955b806486
Rohit Singh <rohitsinghnegi910@gmail.com>
2025-10-22 20:54:03 +0530

    Add PersistGate for wrapping app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 import {Provider} from 'react-redux'
-import store from './store/store.js'
+import store, { persistor } from './store/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 createRoot(document.getElementById('root')).render(
     
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
     </Provider>
 )
