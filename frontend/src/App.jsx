1c17de774809648b6026ac6d1fdc076f10de9f4b
Rohit Singh <rohitsinghnegi910@gmail.com>
2025-10-23 19:19:24 +0530

    Configured routes to show register/logout page based on user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 
 function App() {
+
+  const { user } = useSelector((state) => state.user);
+
+  const isAuthenticated = user && user !== "null";
+
     return (
       <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,_#1E3A8A,_#0F172A,_#000)] text-white antialiased">
         <Routes>
-          <Route path="/" element = {<HomePage />} />
-          <Route path="/login" element = {<Login />} />
-          <Route path="/register" element = {<Register />} />
+          <Route path="/" element = { isAuthenticated ? <HomePage /> : <Navigate to= "/login" /> } /> 
+          <Route path="/login" element = { !isAuthenticated ? <Login /> : <Navigate to='/' />} />
+          <Route path="/register" element = { !isAuthenticated ? <Register /> : <Navigate to= '/' />} />
         </Routes>
       </div>
     )
