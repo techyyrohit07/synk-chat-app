03766efa098717ac10ca49d8791af47757a07c95
Rohit Singh <rohitsinghnegi910@gmail.com>
2025-10-30 13:40:43 +0530

    Update client side URLs

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -7,7 +7,7 @@ let onlineUsers = new Map()
 export const initializeSocketConnection = (server) => {
     io = new Server(server,{
         cors : {
-            origin : "http://localhost:5173",
+            origin : process.env.CLIENT_URL,
             credentials : true
         }
     })
